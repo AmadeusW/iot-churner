dc9232ee5d722fa100ec62db6cc81cd4e4d14ca9
Amadeusz Wieczorek <git@amadeusw.com>
2016-12-13 23:41:04 -0800

    work work

--- client/client.ino
+++ client/client.ino
@@ -1,6 +1,5 @@
 #include <SPI.h>
 #include <WiFi101.h>
-#include <SparkFun_TB6612.h>
 
 // State of LEDs
 int ledR = LOW;
@@ -29,8 +28,6 @@ int buttonState;
 int lastButtonState;
 const unsigned long debounceDelay = 50;
 unsigned long debounceTime = 0;
-// Motor
-Motor motor;
 // WiFi
 int status = WL_IDLE_STATUS;
 WiFiClient client;
@@ -44,12 +41,12 @@ char server[] = "..."; // "your.hostname.com"
 */
 
 void setup() {
-	SetupState();
-	SetupWifi();
-	UpdateState(DOWNLOADING);
-	DownloadRecipe();
-	UpdateState(READY);
-    UpdateUi(0);
+    SetupState();
+    SetupWifi();
+    UpdateState(DOWNLOADING);
+    DownloadRecipe();
+    UpdateState(READY);
+  UpdateUi(0);
 }
 
 void loop() {
@@ -57,7 +54,7 @@ void loop() {
 
     if (state == ERROR)
     {
-		UpdateUi(now);
+        UpdateUi(now);
         delay(100);
         return;
     }
@@ -77,9 +74,9 @@ void loop() {
             buttonState = reading;
 
             if (buttonState == HIGH) {
-                state = CHURNING;
+                UpdateState(CHURNING);
             } else {
-                state = READY;
+                UpdateState(READY);
             }
         }
     }
@@ -89,139 +86,156 @@ void loop() {
 }
 
 void UpdateUi(long millis) {
-	ledR = 0;
-	ledG = 0;
-	ledB = 0;
-	if (state == UNINITIALIZED)
-	{
-		ledR = 140;
-	}
-	else if (state == DOWNLOADING)
-	{
-		ledB = 50;
-	}
-	else if (state == READY)
-	{
-		ledG = 80;
-	}
-	else if (state == CHURNING)
-	{
-		int brightness = 0;
-		if (millis - debounceTime < 10000)
-		{
-			brightness = 100 - (millis - debounceTime) / 100;
-		}
-		ledR = brightness * 1.5;
-		ledG = brightness * 0.5;
-		ledB = brightness * 0.8;
-	}
-	else if (state == ERROR)
-	{
-		// TODO: figure out how to blink LED depending on millis and errorCode
-		if (millis % 500 < 250) {
-			ledR = 200;
-		}
-	}
-	analogWrite(pinLedR, ledR);
-	analogWrite(pinLedG, ledG);
-	analogWrite(pinLedB, ledB);
+    ledR = 0;
+    ledG = 0;
+    ledB = 0;
+    if (state == UNINITIALIZED)
+    {
+        ledR = 140;
+    }
+    else if (state == DOWNLOADING)
+    {
+        ledB = 50;
+    }
+    else if (state == READY)
+    {
+        ledG = 80;
+    }
+    else if (state == CHURNING)
+    {
+        int brightness = 0;
+        if (millis - debounceTime < 10000)
+        {
+            brightness = 100 - (millis - debounceTime) / 100;
+        }
+        ledR = brightness * 1.5;
+        ledG = brightness * 0.5;
+        ledB = brightness * 0.8;
+    }
+    else if (state == ERROR)
+    {
+        // TODO: figure out how to blink LED depending on millis and errorCode
+        if (millis % 500 < 250) {
+            ledR = 200;
+        }
+    }
+    analogWrite(pinLedR, ledR);
+    analogWrite(pinLedG, ledG);
+    analogWrite(pinLedB, ledB);
 }
 
 void Run()
 {
-	motor.drive(100, 500);
-	motor.drive(255, 500);
-	motor.brake();
+    // Start
+    digitalWrite(pinMotorStandby, HIGH);
+    // Go forward
+    digitalWrite(pinMotorIn1, HIGH);
+    digitalWrite(pinMotorIn2, LOW);
+    // Set speeds
+    analogWrite(pinMotorPwm, 100);
+    delay(500);
+    analogWrite(pinMotorPwm, 255);
+    delay(500);
+    
+    // Brake
+    digitalWrite(pinMotorIn1, HIGH);
+    digitalWrite(pinMotorIn2, HIGH);
+    analogWrite(pinMotorPwm, 0);	
 }
 
 void UpdateState(int newState)
 {
-	state = newState;
+    if (!Serial)
+        Serial.println(newState);
+    state = newState;
 }
 
 void Error(int code)
 {
-	errorCode = code;
-	UpdateState(ERROR);
+    errorCode = code;
+    UpdateState(ERROR);
 }
 
 void SetupState() {
-	pinMode(pinButton, INPUT);
-	pinMode(pinLedR, OUTPUT);
-	pinMode(pinLedG, OUTPUT);
-	pinMode(pinLedB, OUTPUT);
-	digitalWrite(pinLedR, ledR);
-	digitalWrite(pinLedG, ledG);
-	digitalWrite(pinLedB, ledR);
-	motor = Motor(pinMotorIn1, pinMotorIn2, pinMotorPwm, 1, pinMotorStandby);
+    pinMode(pinButton, INPUT);
+    pinMode(pinLedR, OUTPUT);
+    pinMode(pinLedG, OUTPUT);
+    pinMode(pinLedB, OUTPUT);
+    digitalWrite(pinLedR, ledR);
+    digitalWrite(pinLedG, ledG);
+    digitalWrite(pinLedB, ledR);
+    pinMode(pinMotorIn1, OUTPUT);
+    pinMode(pinMotorIn2, OUTPUT);
+    pinMode(pinMotorPwm, OUTPUT);
+    pinMode(pinMotorStandby, OUTPUT);
 }
 
 void SetupWifi() {
-	//Initialize serial and wait for port to open
-	//TODO: See if I need this for untethered operation
-	Serial.begin(9600);
-	while (!Serial) {
-		;
-	}
+    //Initialize serial and wait for port to open
+    //TODO: See if I need this for untethered operation
+    Serial.begin(9600);
+    while (!Serial) {
+        ;
+    }
 
-	// check for the presence of the shield:
-	if (WiFi.status() == WL_NO_SHIELD) {
-		Serial.println("WiFi shield not present");
-		while (true);
-	}
+    // check for the presence of the shield:
+    if (WiFi.status() == WL_NO_SHIELD) {
+        Serial.println("WiFi shield not present");
+        while (true);
+    }
 
-	// attempt to connect to Wifi network:
-	while (status != WL_CONNECTED) {
-		Serial.print("Attempting to connect to SSID: ");
-		Serial.println(ssid);
-		status = WiFi.begin(ssid, pass);
-		delay(2000);
-	}
-
-	Serial.println("Connected to wifi");
-	printWifiStatus();
+    delay(1000);
+    // attempt to connect to Wifi network:
+    while (status != WL_CONNECTED) {
+        Serial.print("Attempting to connect to SSID: ");
+        Serial.println(ssid);
+        status = WiFi.begin(ssid, pass);
+        delay(1000);
+    }
+    printWifiStatus();
 }
 
 void DownloadRecipe() {
-	Serial.println("\nStarting connection to server...");
-	// if you get a connection, report back via serial:
-	if (client.connect(server, 80)) {
-		Serial.println("connected to server");
-		// Make a HTTP request:
-		client.println(request);
-		client.println("Host: amadeusw-iot.azurewebsites.net");
-		client.println("Connection: close");
-		client.println();
-	}
+    Serial.println("\nStarting connection to server...");
+    // if you get a connection, report back via serial:
+    if (client.connect(server, 80)) {
+        Serial.println("connected to server");
+        // Make a HTTP request:
+        client.println(request);
+        Serial.println(request);
+        client.println("Host: amadeusw-iot.azurewebsites.net");
+        client.println("Connection: close");
+        client.println();
+    }
 
-	// if there are incoming bytes available
-	// from the server, read them and print them:
-	while (client.available()) {
-		char c = client.read();
-		Serial.write(c);
-	}
+    // if there are incoming bytes available
+    // from the server, read them and print them:
+    while (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+    }
 
-	// if the server's disconnected, stop the client:
-	if (!client.connected()) {
-		Serial.println();
-		Serial.println("disconnecting from server.");
-		client.stop();
-	}
+    // if the server's disconnected, stop the client:
+    if (!client.connected()) {
+        Serial.println();
+        Serial.println("disconnecting from server.");
+        client.stop();
+    }
 }
 
 void printWifiStatus() {
-	// print the SSID of the network you're attached to:
-	Serial.print("SSID: ");
-	Serial.println(WiFi.SSID());
+    // print the SSID of the network you're attached to:
+    Serial.print("SSID: ");
+    Serial.println(WiFi.SSID());
 
-	// print your WiFi shield's IP address:
-	IPAddress ip = WiFi.localIP();
-	Serial.print("IP Address: ");
-	Serial.println(ip);
+    // print your WiFi shield's IP address:
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
 
-	// print the received signal strength:
-	long rssi = WiFi.RSSI();
-	Serial.print("signal strength (RSSI):");
-	Serial.print(rssi);
-	Serial.println(" dBm");
+    // print the received signal strength:
+    long rssi = WiFi.RSSI();
+    Serial.print("signal strength (RSSI):");
+    Serial.print(rssi);
+    Serial.println(" dBm");
 }
