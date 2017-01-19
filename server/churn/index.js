d9cc6b46887a830354e4f33e110ddfd37ece3b84
Amadeusz Wieczorek <git@amadeusw.com>
2017-01-19 00:05:26 -0800

    server: send total time

--- server/churn/index.js
+++ server/churn/index.js
@@ -1,10 +1,12 @@
 module.exports = function(context, req) {
 
     var payload1 = {
+        "TotalTime": 30000,
         "Times":    [ 0,  500,    2500,   3000,   4000,   4500,   6500,   7000 ],
         "Speeds":   [ 0,  100,    100,    0,      0,      -100,   -100,   0    ]
     };
     var payload2 = {
+        "TotalTime": 10000,
         "Times":    [ 0,  1000,    2000,   3000,   4000 ],
         "Speeds":   [ 0,  100,    -100,    100,      -100]
     };
@@ -24,6 +26,8 @@ function serialize(payload1, payload2)
     var dataSize = payload1["Times"].length;
     var dataSize2 = payload2["Times"].length;
     var message = ";;;";
+    message += payload1["TotalTime"];
+    message += ";";
     for (var i = 0; i < dataSize; i++) {
         message += payload1["Times"][i];
         message += ":";
@@ -31,6 +35,8 @@ function serialize(payload1, payload2)
         message += ";";
     }
     message += ".";
+    message += payload2["TotalTime"];
+    message += ";";
     for (var i = 0; i < dataSize2; i++) {
         message += payload2["Times"][i];
         message += ":";
